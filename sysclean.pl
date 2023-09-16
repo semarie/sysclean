01ee455ebe72eb19cc8cd35333f3bac5d48c46cc
Sebastien Marie <semarie@kapouay.eu.org>
2023-09-16 09:07:52 +0000

    drop SSH DSA keys
    DSA was disabled by default eight years ago, and ssh-keygen -A belatedly stopped 
    generating DSA keys 15 months ago.
    
    Suggested by naddy@

--- sysclean.pl
+++ sysclean.pl
@@ -178,8 +178,6 @@ sub add_expected_base
 		'/etc/kbdtype' => 1,
 		'/etc/ssh/ssh_host_rsa_key' => 1,
 		'/etc/ssh/ssh_host_rsa_key.pub' => 1,
-		'/etc/ssh/ssh_host_dsa_key' => 1,
-		'/etc/ssh/ssh_host_dsa_key.pub' => 1,
 		'/etc/ssh/ssh_host_ecdsa_key' => 1,
 		'/etc/ssh/ssh_host_ecdsa_key.pub' => 1,
 		'/etc/ssh/ssh_host_ed25519_key' => 1,
